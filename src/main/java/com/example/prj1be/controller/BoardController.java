99b3c615b9eb53ebe5336112572bc99f8918adb7
kkssii0210 <kkssii0210@naver.com>
2023-11-09 11:45:33 +0900

    first commit

---
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.example.prj1be.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
+public class BoardController {
+    @PostMapping("add")
+    public void add() {
+
+    }
+}
