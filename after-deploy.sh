5d3a3fd666ec53ae5e3de1348d7b7ad70e21ad61
kkssii0210 <kkssii0210@naver.com>
2023-12-04 17:19:50 +0900

    CI/CD 작성중2

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
-REPOSITORY=/home/api/
+#!/usr/bin/env bash
+REPOSITORY=/home/ubuntu/prj1-be
+cd $REPOSITORY
+APP_NAME=prj1-be
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-cd $REPOSITORY/api_back
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
